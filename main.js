eb27ade9fed7ad4a1ad5b0f2fac6908615c56000
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2019-07-22 21:12:46 +0900

    わかりやすさのため signalingUrl を先頭に定義する

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const qs = window.Qs;
-const signalingUrl = 'wss://ayame.shiguredo.jp/ws'
+const signalingUrl = 'wss://ayame.shiguredo.jp/ws';
 let roomId = 'ayame-test-room';
 let clientId = null;
+
 // query string から roomId, clientId を取得するヘルパー
 function parseQueryString() {
+  const qs = window.Qs;
   if (window.location.search.length > 0) {
     var params = qs.parse(window.location.search.substr(1));
     if (params.roomId) {
